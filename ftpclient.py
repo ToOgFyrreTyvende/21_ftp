51e70281d6e19d7f84ad142a6782316d58321925
Ahmad Sattar <thehabbos007@gmail.com>
2019-03-12 16:14:50 +0100

    Minor changes to code + comments

--- ftpclient.py
+++ ftpclient.py
@@ -43,11 +43,12 @@ class FTPClient():
     def login(self):
         user = 'anonymous'
         passwd = 'anonymous@'
-        acct = ''
 
         resp = self.send_cmd('USER ' + user)
+        # We check for a positive status code
         if resp[0] == '3':
             resp = self.send_cmd('PASS ' + passwd)
+        # We check for a positive status code again, we raise an error if itøs not a 2xx FTP status code
         if resp[0] != '2':
             raise ValueError("Could not log in! " + str(resp))
         return resp
@@ -65,72 +66,87 @@ class FTPClient():
 
     def getline(self):
         line = self.file.readline(MAXBYTES)
-        if len(line) > MAXBYTES:
-            raise ValueError("got more than %d bytes" % MAXBYTES)
         if not line:
-            raise EOFError
+            raise ValueError("Line is not really a line :(")
+        # IF the last 2 elements from the end is a carriage return line feed, 
+        # we set the retrieved line as everything but the CRLF
         if line[-2:] == CRLF:
             line = line[:-2]
+        # Even if we just have either a carriage return or new line, 
+        # we do the same as the above
         elif line[-1:] in CRLF:
             line = line[:-1]
         return line
 
     def getmultiline(self):
         line = self.getline()
+        # first line will begin with the exact required reply code, 
+        # followed immediately by a Hyphen, "-". Therefore, we check for that at text index 3
         if line[3:4] == '-':
+            # The status code is everything up untill index 3
             code = line[:3]
             while 1:
+                # Of course we sill get next lines one by one
                 nextline = self.getline()
+                # Line will contian a proper representation of the whole data stream
                 line = line + ('\n' + nextline)
-                if nextline[:3] == code and \
-                        nextline[3:4] != '-':
+                # If we somehow encounter a new status code wit ha hyphen, we are at the next 
+                # data response, so we break
+                if nextline[:3] == code and nextline[3:4] != '-':
                     break
+        # line wil contain all lines in the response concatenated by a \n new line escaped char.
         return line
 
+    # we wrap our final message in this getresp method.
     def getresp(self):
         resp = self.getmultiline()
         c = resp[:1]
+        # Since we know status codes 1, 2 and 3 are generally "good"
         if c in {'1', '2', '3'}:
             return resp
-        if c == '4':
-            raise ValueError(resp)
-        if c == '5':
-            raise ValueError(resp)
+
         raise ValueError(resp)
 
     def retrlines(self, cmd):
         resp = self.send_cmd('TYPE A')
-        with self.transfercmd(cmd) as conn, \
-                 conn.makefile('r', encoding="latin1") as fp:
+        with self.transfercmd(cmd) as conn, conn.makefile('r', encoding="latin1") as fp:
             acc_bytes = 1
+            split_cmd = cmd.split()
+            if len(split_cmd) == 2:
+                print("Starting file print of file " + split_cmd[1])
             while 1:
                 line = fp.readline(MAXBYTES)
-                if len(line) > MAXBYTES:
-                    raise ValueError("got more than %d bytes" % MAXBYTES)
                 if not line or acc_bytes > MAXBYTES:
                     break
-                print('*retr*', repr(line)[0:(MAXBYTES-acc_bytes)])
+                # If the accumulative number of bytes does not exceed 1024 (maxbytes)
+                # we print the file contents to terminal
+                if acc_bytes <= MAXBYTES:
+                    print(repr(line)[0:(MAXBYTES-acc_bytes)])
+                # Same logic as in getline. We check if there is a line ending in the current data stream 
                 if line[-2:] == CRLF:
                     line = line[:-2]
                 elif line[-1:] == '\n':
                     line = line[:-1]
+                # We have an accumulative byte variable to keep track of what we need to print
                 acc_bytes += len(line)
-            # shutdown ssl layer
         return self.voidresp()
 
     def transfercmd(self, cmd):
+        # We make sure we follow up on the Data connection creation that the 
+        # server negotiates
         with self.makeport() as sock:
             resp = self.send_cmd(cmd)
             # See above.
             if resp[0] == '2':
                 resp = self.getresp()
             if resp[0] != '1':
-                raise error_reply(resp)
+                raise ValueError(resp)
             conn, _ = sock.accept()
         return conn
 
+    #This method is not changed from FTPLib.
     def makeport(self):
-        '''Create a new socket and send a PORT command for it.'''
+        #Create a new socket and send a PORT command for it.
         err = None
         sock = None
         for res in socket.getaddrinfo(None, 0, socket.AF_INET, socket.SOCK_STREAM, 0, socket.AI_PASSIVE):
@@ -157,10 +173,9 @@ class FTPClient():
         resp = self.sendport(host, port)
         return sock
 
+    # This method is also borrowed from FTPLib
     def sendport(self, host, port):
-        '''Send a PORT command with the current host and the given
-        port number.
-        '''
+        #Send a PORT command with the current host and the given port number.
         hbytes = host.split('.')
         pbytes = [repr(port//256), repr(port%256)]
         bytes = hbytes + pbytes
@@ -178,6 +193,7 @@ class FTPClient():
         return resp
 
     def storlines(self, cmd, fp):
+        # we set transfer type to Ascii with the "TYPE" FTP command
         self.voidcmd('TYPE A')
         with self.transfercmd(cmd) as conn:
             while 1:
@@ -186,6 +202,7 @@ class FTPClient():
                     raise Error("got more than %d bytes" % MAXBYTES)
                 if not buf:
                     break
+                # Here B_CRLF is used, which is a binary string that can be used in place of a byte type
                 if buf[-2:] != B_CRLF:
                     if buf[-1].encode('ascii') in B_CRLF: buf = buf[:-1]
                     buf = buf.encode('ascii') + B_CRLF
